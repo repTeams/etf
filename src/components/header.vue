efc388a9ee322dde19b2f8bbf1daca91e61938df
fashandian <860482011@qq.com>
2019-08-30 16:48:24 +0800

    header component

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,24 +1,37 @@
 <!--
  * @Date: 2019-08-29 22:30:22
  * @LastEditors: fashandian
- * @LastEditTime: 2019-08-30 01:58:05
+ * @LastEditTime: 2019-08-30 16:47:15
 -->
 <template>
-    <header class="header clearfloat">
+    <header class="header">
         <div class="header-wrap">
-            <div class="header-menu float-left"></div>
-            <div class="header-logo float-left"></div>
-            <div class="header-lang float-right">
-                <p>简</p>
-                <ul>
-                    <li @click="changeLand('zh')">简</li>
-                    <li @click="changeLand('tr')">繁</li>
-                    <li @click="changeLand('en')">EN</li>
+            <div class="header-left">
+                <div class="icon-header-menu"></div>
+                <div class="header-logo-mobile"></div>
+                <img class="header-logo-pc" src="../static/img/logo.png"
+                    alt="logo" srcset="../static/img/logo@2x.png 2x">
+                <ul class="header-menu">
+                    <li>首页</li>
+                    <li>公司介绍</li>
+                    <li>新闻媒体</li>
+                    <li>联系我们</li>
+                    <li>战略合作伙伴</li>
                 </ul>
             </div>
-            <el-row class="float-right">
-                <el-button class="btn-register" round>{{$t('home.register')}}</el-button>
-            </el-row>
+            <div class="header-right">
+                <el-row >
+                    <el-button class="btn-register" round>{{$t('home.register')}}</el-button>
+                </el-row>
+                <div class="header-lang">
+                    <p>简</p>
+                    <ul>
+                        <li @click="changeLand('zh')">简</li>
+                        <li @click="changeLand('tr')">繁</li>
+                        <li @click="changeLand('en')">EN</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </header>
 </template>
@@ -45,29 +58,83 @@ export default {
         position: absolute;
         z-index: 10;
         .header-wrap {
-            max-width: 1200px;
+            max-width: 1200PX;
             margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
         }
-        .header-menu {
+        .header-left {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+        }
+        .header-right {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+        }
+        .icon-header-menu {
             width: 52px;
             height: 52px;
             background-image: url('../static/img/home/icon_menu.png');
             background-repeat: no-repeat;
             background-size: 52px 52px;
             cursor: pointer;
+            margin-right: 40px;
         }
-        .header-logo {
+        .header-menu {
+            display: none;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 1PX solid #17FFFF;
+            padding-top: 8PX;
+            // padding-bottom: 24PX;
+            margin-left: 37PX;
+            > li {
+                color: #fff;
+                margin-right: 54PX;
+                cursor: pointer;
+                position: relative;
+                padding-bottom: 24PX;
+                &.selected, &:hover {
+                    color: #17FFFF;
+                }
+                &::before {
+                    content: "";
+                    position: absolute;
+                    top: -2PX;
+                    right: 100%;
+                    width: 0;
+                    height: 100%;
+                    border-bottom: 4PX solid #17FFFF;
+                    transition: 0.2s all linear;
+                    border-radius: 2PX;
+                }
+                &:hover::before {
+                    width: 100%;
+                    right: 0;
+                }
+                &:hover ~ &::before {
+                    right: 0;
+                }
+            }
+            
+        }
+        .header-logo-pc {
+            display: none;
+        }
+        .header-logo-mobile {
             width: 163px;
             height: 58px;
             background-image: url('../static/img/logo.png');
             background-repeat: no-repeat;
             background-size: 163px 58px;
             cursor: pointer;
-            margin-left: 40px;
         }
         .btn-register {
             padding: 13px 25px;
-            border: 1px solid rgba(255,255,255,.6);
+            border: 1PX solid rgba(255,255,255,.6);
             font-size: 28px;
             background: rgba(9,14,61,.6);
             border-radius: 26px;
@@ -126,11 +193,60 @@ export default {
     /* 如果设备像素大于等于2，则用2倍图 */
     @media only screen and (-webkit-min-device-pixel-ratio: 2),
     screen and (min--moz-device-pixel-ratio: 2) {
-        .header-menu {
+        .icon-header-menu {
             background-image: url('../static/img/home/icon_menu@2x.png');
         }
-        .header-logo {
+        .header-logo-mobile {
             background-image: url('../static/img/logo@2x.png');
+        }
+    }
+
+    @media only screen and (min-width: 750px) {
+        [data-dpr="1"] .header {
+            background: transparent;
+            font-size: 18PX;
+            .btn-register, .header-lang p, .header-lang ul li {
+                font-size: 18PX;
+            }
+            .icon-header-menu {
+                display: none;
+            }
+            .header-menu {
+                display: flex;
+            }
+            .header-logo-mobile {
+                display: none;
+            }
+            .header-logo-pc {
+                display: block;
+            }
+        }
+    }
+    @media only screen and (max-width: 1030px) {
+        [data-dpr="1"] .header {
+            font-size: 16PX;
+            .btn-register, .header-lang p, .header-lang ul li {
+                font-size: 16PX;
+            }
+            .header-menu {
+                margin-left: 30PX;
+                > li {
+                    margin-right: 30PX;
+                }
+            }
+        }
+    }
+    @media only screen and (max-width: 860px) {
+        [data-dpr="1"] .header {
+            .header-menu {
+                margin-left: 15PX;
+                > li {
+                    margin-right: 15PX;
+                    &:last-child {
+                        margin-right: 0;
+                    }
+                }
+            }
         }
     }
 </style>
